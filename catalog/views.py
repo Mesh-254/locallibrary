10611ecae01459339faaade56e71d3189930a752
mesh-254 <meshack1995@gmail.com>
2022-12-12 17:50:20 +0300

    Created forms to create, update and delete authots

--- catalog/views.py
+++ catalog/views.py
@@ -1,17 +1,13 @@
-from http.client import HTTPResponse
-
-from django.contrib.admin.views.decorators import staff_member_required
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Book, Author, BookInstance, Genre
-from django.contrib.auth.decorators import login_required
+from .models import Book, Author, BookInstance
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.contrib.auth.decorators import login_required, permission_required
 import datetime
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from catalog.forms import RenewBookForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 
 # Create your views here.
@@ -146,3 +142,19 @@ def renew_book_librarian(request, pk):
     }
 
     return render(request, 'catalog/book_renew_librarian.html', context)
+
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_death': '11/06/2020'}
+
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = '__all__'  # Not recommended (potential security issue if more fields added)
+
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
